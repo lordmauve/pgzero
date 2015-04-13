a41e091709c9c1068a315875813b6b4d11819ce4
Daniel Pope <mauve@mauveweb.co.uk>
2015-04-13 17:58:53 -0400

    Improve the clarity of the demo2 code

--- examples/demo2.py
+++ examples/demo2.py
@@ -1,33 +1,33 @@
 from images import alien
 
 TITLE = "Alien walk"
-
 WIDTH = 500
 HEIGHT = alien.get_height() + 20
 
-x = 50
 
-
-def alien_position():
-    """Get the current position of the alien."""
-    return (x, 10)
+# The rectangle in which the alien will be drawn
+alien_rect = alien.get_rect().move((-50, 10))
 
 
 def draw(screen):
     """Clear the screen and draw the alien."""
     screen.fill((0, 0, 0))
-    screen.blit(alien, alien_position())
+    screen.blit(alien, alien_rect)
 
 
 def update(dt):
     """Move the alien by one pixel."""
-    global x, HEIGHT
-    x += 1
-    if x > WIDTH:
-        x = -100
+    alien_rect.x += 1
+
+    # If the alien is off the right hand side of the screen,
+    # move it back off screen to the left-hand side
+    if alien_rect.x > WIDTH:
+        alien_rect.x = -100
 
 
 def on_mouse_down(pos):
-    bounds = alien.get_rect().move(*alien_position())
-    if bounds.collidepoint(pos):
+    """Detect clicks on the alien."""
+    if alien_rect.collidepoint(pos):
         print("You clicked the alien!")
+    else:
+        print("You missed the alien!")
