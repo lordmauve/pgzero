43c9f539b28cff2b3f505b64484a1c2abd748e32
Daniel Pope <mauve@mauveweb.co.uk>
2015-04-13 18:09:31 -0400

    Add demo demonstrating use of clock

--- pgzero/imageloader.py
+++ pgzero/imageloader.py
@@ -13,12 +13,26 @@ class ImageLoaderModule(ModuleType):
     def __getattr__(self, name):
         subdir = os.path.join(self.path, name)
         if os.path.isdir(subdir):
-            return ImageLoaderModule(subdir)
+            resource = ImageLoaderModule(subdir)
+        else:
+            try:
+                resource = self.load(name)
+            except KeyError as e:
+                raise AttributeError(*e.args)
+
+        setattr(self, name, resource)
+        return resource
+
+    def load(self, name):
+        p = os.path.join(self.path, name)
+        if os.path.exists(p):
+            return self.load_(p)
 
         for ext in self.EXTNS:
             p = os.path.join(self.path, name + '.' + ext)
             if os.path.exists(p):
                 return self.load_(p)
+        raise KeyError("No image found like '%s'" % name)
 
     def load_(self, path):
         return pygame.image.load(path)
