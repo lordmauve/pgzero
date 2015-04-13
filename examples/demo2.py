d3f68bfb8c8f5479f08000dc1a4e722d462bc674
Daniel Pope <mauve@mauveweb.co.uk>
2015-04-13 18:41:13 -0400

    Simplify and document the demos slightly

--- examples/demo2.py
+++ examples/demo2.py
@@ -5,29 +5,22 @@ WIDTH = 500
 HEIGHT = alien.get_height() + 20
 
 
-# The rectangle in which the alien will be drawn
-alien_rect = alien.get_rect().move((-50, 10))
+# The horizontal position of the alien
+x = -50
 
 
 def draw(screen):
     """Clear the screen and draw the alien."""
     screen.fill((0, 0, 0))
-    screen.blit(alien, alien_rect)
+    screen.blit(alien, (x, 10))
 
 
 def update(dt):
     """Move the alien by one pixel."""
-    alien_rect.x += 1
+    global x
+    x += 1
 
     # If the alien is off the right hand side of the screen,
     # move it back off screen to the left-hand side
-    if alien_rect.x > WIDTH:
-        alien_rect.x = -100
-
-
-def on_mouse_down(pos):
-    """Detect clicks on the alien."""
-    if alien_rect.collidepoint(pos):
-        print("You clicked the alien!")
-    else:
-        print("You missed the alien!")
+    if x > WIDTH:
+        x = -100
--- examples/demo3.py
+++ examples/demo3.py
@@ -17,8 +17,9 @@ def draw(screen):
     screen.blit(sprite, alien_rect.topleft)
 
 
-def update(dt):
-    """Move the alien by one pixel."""
+def update():
+    """Move the alien."""
+    # Move the alien one pixel to the right
     alien_rect.x += 1
 
     # If the alien is off the right hand side of the screen,
@@ -29,18 +30,23 @@ def update(dt):
 
 def on_mouse_down(pos):
     """Detect clicks on the alien."""
-    global sprite
     if alien_rect.collidepoint(pos):
         set_alien_hurt()
 
 
 def set_alien_hurt():
+    """Set the current alien sprite to the "hurt" image."""
     global sprite
     sprite = alien_hurt
+    # The alien may already be shown as hurt. In this case we need to prolong
+    # the time it is shown as hurt. Calling unschedule() will cancel the
+    # previous scheduled recovery if there was one. Then we can schedule the
+    # alien to recover 1 second from now.
     unschedule(set_alien_normal)
     schedule(set_alien_normal, 1.0)
 
 
 def set_alien_normal():
+    """Set the current alien sprite to the normal image."""
     global sprite
     sprite = alien
