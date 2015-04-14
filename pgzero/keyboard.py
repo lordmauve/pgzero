342f19888cea60617f7f74c1827911b5c3070dc4
Richard Jones <663366+r1chardj0n3s@users.noreply.github.com>
2015-04-14 16:08:27 -0400

    fix

--- pgzero/keyboard.py
+++ pgzero/keyboard.py
@@ -3,25 +3,24 @@ from collections import defaultdict
 from pygame import locals
 
 
-class Keyboard(object):
+class Keyboard:
     def __init__(self):
-        self.d = defaultdict(lambda: False)
+        self.__dict__['d'] = defaultdict(lambda: False)
 
     def __getattr__(self, key):
-        if key.startswith('K_'):
-            key = getattr(locals, key)
-        else:
-            key = getattr(locals, 'K_' + key)
-        return self.d[key]
+        return self[key]
 
     def __setattr__(self, key, value):
-        self.d[key] = value
+        self[key] = value
 
     def __getitem__(self, key):
-        if key.startswith('K_'):
-            key = getattr(locals, key)
-        else:
-            key = getattr(locals, 'K_' + key)
+        try:
+            if key.startswith('K_'):
+                key = getattr(locals, key)
+            else:
+                key = getattr(locals, 'K_' + key)
+        except AttributeError:
+            raise AttributeError('the key "%s" does not exist' % key)
         return self.d[key]
 
     def __setitem__(self, key, value):
