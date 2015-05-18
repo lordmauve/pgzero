b48a350ba31c4a46d9a4b58285b7bd34e40aadf7
Daniel Pope <mauve@mauveweb.co.uk>
2015-05-19 00:13:54 +0100

    Fix: make Actor.image behave unsurprisingly

--- pgzero/actor.py
+++ pgzero/actor.py
@@ -7,21 +7,24 @@ from . import loaders
 class Actor(pygame.rect.Rect):
     def __init__(self, image, pos=(0, 0)):
         self.image = image
-        super(Actor, self).__init__(pos, self.image.get_size())
+        super(Actor, self).__init__(pos, self._surf.get_size())
 
     @property
     def pos(self):
         return self.topleft
+
     @pos.setter
     def pos(self, pos):
         self.topleft = pos
 
     @property
     def image(self):
-        return self._image
+        return self._image_name
+
     @image.setter
     def image(self, image):
-        self._image = loaders.images.load(image)
+        self._image_name = image
+        self._surf = loaders.images.load(image)
 
     def draw(self):
-        game.screen.blit(self.image, self.pos)
+        game.screen.blit(self._surf, self.pos)
