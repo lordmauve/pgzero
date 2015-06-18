39d9cb2751996ced6e63be861f75de081c3ad1fe
Tim Golden <mail@timgolden.me.uk>
2015-06-18 20:50:21 +0100

    Rename the new Rect to ZRect

--- pgzero/actor.py
+++ pgzero/actor.py
@@ -35,7 +35,7 @@ def calculate_anchor(value, dim, total):
 TOPLEFT = None  # symbolic name for the default positioning of the Actor
 
 
-class Actor(rect.Rect):
+class Actor(rect.ZRect):
     _anchor = _anchor_value = (0, 0)
 
     def __init__(self, image, pos=TOPLEFT, anchor=('center', 'center')):
