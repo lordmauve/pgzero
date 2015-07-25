d9a39a982638438064a80db96bcd7f076c2bc305
Rob Collins <rob.collins@agiledata.io>
2015-07-25 18:27:09 +0200

    Issue #1: Further experiments

--- pgzero/runner.py
+++ pgzero/runner.py
@@ -12,12 +12,42 @@ from .game import PGZeroGame, DISPLAY_FLAGS
 from . import loaders
 from . import builtins
 
+import sys, os
+os.environ
+
 
 def main():
-    print(sys.executable)
+    print("sys.executable = {}".format(sys.executable))
+    # if '.virtualenvs' in sys.executable: # TO-DO better test
+    # Fix only for os 10
 
-    os.execv('')
-    assert 5 == 6
+    if not 'Library/Frameworks' in sys.executable:
+        print("***Changing the executable***")
+        PYVER = '3.4'
+        base_fw = '/Library/Frameworks/Python.framework/Versions/'
+        real_python = base_fw + '{pv}/bin/python{pv}'.format(pv=PYVER)
+        print(real_python)
+        print(sys.argv)
+        print(99)
+        # old_python = '/Users/rcollin2/.virtualenvs/pgzero/bin/python'
+
+        venv_paths = [p for p in sys.path
+                      if p.startswith(os.environ['VIRTUAL_ENV'])]
+        os.environ['PYTHONPATH'] = ':'.join(venv_paths + [
+            os.environ['PYTHONPATH']])
+
+        # from pkg_resources import load_entry_point
+        # sys.exit(
+        #     load_entry_point('pgzero==1.1rc1', 'console_scripts', 'pgzrun')()
+        # )
+        print('venv_paths')
+        print(venv_paths)
+        # os.execv(old_python, ['pgzrun'])  #sys.argv)
+        # os.execv(real_python, ['python', '/Users/rcollin2/.virtualenvs/pgzero/bin/pgzrun'] + sys.argv[1:])  #sys.argv)
+        os.execv(real_python, ['python', '-m', 'pgzero'] + sys.argv[1:])  #sys.argv)
+        # os.execv(real_python, ['python', 'pgzrun'] + sys.argv[1:])  #sys.argv)
+        assert 99 == 77
+    # assert 5 == 6
 
 
     parser = OptionParser()
