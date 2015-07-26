a0684d7628aa74f812c535fc0b8d2d7ae96dc49d
Ivan Mesic <navi7@users.noreply.github.com>
2015-07-26 14:52:37 +0200

    Basic AI for computer

--- examples/pong/pong.py
+++ examples/pong/pong.py
@@ -83,6 +83,7 @@ class Game():
         self.score_right = 0
 
         self.in_progress = False
+        self.computer_acting = False
 
         # position paddles in the middle of the screen
         middle = HEIGHT/2 - PADDLE_HEIGHT/2
@@ -110,11 +111,13 @@ class Game():
 
     def score_for_left(self):
         self.in_progress = False
+        self.active_player = LEFT_PLAYER
         self.score_left += 1
         self.set_ball(self.ball_pos)
 
     def score_for_right(self):
         self.in_progress = False
+        self.active_player = RIGHT_PLAYER
         self.score_right += 1
         self.set_ball(self.ball_pos)
 
@@ -144,6 +147,71 @@ class Game():
 
         if self.score_left == 11 or self.score_right == 11:
             self.in_progress = False
+
+    # computer movement
+    def computer_launch(self):
+        self.in_progress = True
+        self.computer_acting = False
+
+    def computer_stop_acting(self):
+        self.computer_acting = False
+
+    def computer_move(self):
+        # move towards the center of the screen when the ball is
+        # travelling toward the enemy
+        if self.tennis_ball.dx > 0:
+            target_y = HEIGHT / 2
+        else:
+            # when the ball is on other side of screen, just move
+            # in general direction
+            if self.tennis_ball.x > WIDTH / 2:
+                delta = int(WIDTH * 0.25)
+                if self.tennis_ball.dy < 0:
+                    target_y = self.tennis_ball.y - delta
+                else:
+                    target_y = self.tennis_ball.y + delta
+            else:
+                # the ball is on our side, move with it
+                rnd = random.randint(40, 200)
+                if self.tennis_ball.dy < 0:
+                    target_y = self.tennis_ball.y - rnd
+                else:
+                    target_y = self.tennis_ball.y + rnd
+
+        target_y = max(40, min(target_y, HEIGHT - 80))
+
+        distance = abs(self.left_paddle.y - target_y)
+        duration = min(1.0, distance / 600.0)
+
+        animate(self.left_paddle, y=target_y, duration=.50,
+            on_finished=self.computer_stop_acting)
+
+    def computer_move_randomly(self):
+        # move the paddle randomly during one second before launching the ball
+        target_y = random.randint(40, HEIGHT - PADDLE_HEIGHT - 80)
+        distance = abs(self.left_paddle.y - target_y)
+        duration = distance / 200.0
+        self.computer_total_duration += duration
+
+        if self.computer_total_duration + duration < 1.0:
+            on_finished = self.computer_move_randomly
+        else:
+            on_finished = self.computer_launch
+
+        animate(self.left_paddle, y=target_y, duration=duration,
+            on_finished=on_finished)
+
+    def computer_act(self):
+        if self.in_progress:
+            # predict where the ball will move and move towards it
+            self.computer_move()
+        elif self.active_player == LEFT_PLAYER:
+            # move randomly for a bit, then shoot the ball out
+            if not self.computer_acting:
+                self.computer_acting = True
+
+                self.computer_total_duration = 0.0
+                self.computer_move_randomly()
 
     def draw(self):
         # slightly gray background
@@ -196,7 +264,10 @@ class Game():
         self.right_paddle.draw()
         self.tennis_ball.draw()
 
-player = LEFT_PLAYER if random.randint(0, 1000) % 2 == 0 else RIGHT_PLAYER
+
+# first player is chosen randomly
+# player = LEFT_PLAYER if random.randint(0, 1000) % 2 == 0 else RIGHT_PLAYER
+player = LEFT_PLAYER
 game = Game(player)
 
 
@@ -209,6 +280,9 @@ def update():
         game.right_paddle.up()
     elif keyboard.down:
         game.right_paddle.down()
+
+    # move the computer controlled paddle
+    game.computer_act()
 
     # set the position of the ball to be in the middle of the paddle
     if not game.in_progress:
