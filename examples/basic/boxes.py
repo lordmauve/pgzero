9486ba6fba540d949dfa4480476b74cf7f14c0ea
Daniel Pope <lordmauve@users.noreply.github.com>
2015-08-04 00:24:01 +0100

    Fix boxes example not to require arbitrary attributes on Rect

--- examples/basic/boxes.py
+++ examples/basic/boxes.py
@@ -2,7 +2,7 @@ WIDTH = 300
 HEIGHT = 300
 
 
-def make_rects():
+def draw():
     r = 255
     g = 0
     b = 0
@@ -10,23 +10,13 @@ def make_rects():
     width = WIDTH
     height = HEIGHT - 200
 
-    rects = []
     for i in range(20):
         rect = Rect((0, 0), (width, height))
-        rect.color = (r, g, b)
         rect.center = 150, 150
-        rects.append(rect)
+        screen.draw.rect(rect, (r, g, b))
+
         r -= 10
         g += 10
 
         width -= 10
         height += 10
-    return rects
-
-
-rects = make_rects()
-
-
-def draw():
-    for r in rects:
-        screen.draw.rect(r, r.color)
