fd76f638ae20709deb44c3516785b43b7211ecde
Daniel Pope <mauve@mauveweb.co.uk>
2015-11-25 00:31:08 +0000

    Simplify code for Flappy Bird a little

--- examples/flappybird/flappybird.py
+++ examples/flappybird/flappybird.py
@@ -4,17 +4,19 @@ TITLE = 'Flappy Bird'
 WIDTH = 400
 HEIGHT = 708
 
-bird = Actor('bird1', (75, 50))
+# These constants control the difficulty of the game
+GAP = 130
+GRAVITY = 0.3
+FLAP_STRENGTH = 6.5
+SPEED = 3
+
+bird = Actor('bird1', (75, 200))
 bird.dead = False
 bird.score = 0
-bird.jump = 0
-bird.jumpSpeed = 10
-bird.gravity = 5
+bird.vy = 0
 
 wall_top = Actor('top', anchor=('left', 'bottom'), pos=(-100, 0))
 wall_bottom = Actor('bottom', anchor=('left', 'top'), pos=(-100, 0))
-
-GAP = 130
 
 
 def reset_walls():
@@ -24,41 +26,35 @@ def reset_walls():
 
 
 def update_walls():
-    wall_top.left -= 3
-    wall_bottom.left -= 3
-    if wall_top.right < -0:
+    wall_top.left -= SPEED
+    wall_bottom.left -= SPEED
+    if wall_top.right < 0:
         reset_walls()
         bird.score += 1
 
 
 def update_bird():
-    if bird.jump:
-        bird.jumpSpeed -= 1
-        bird.top -= bird.jumpSpeed
-        bird.jump -= 1
-    else:
-        bird.top += bird.gravity
-        bird.gravity += 0.2
+    uy = bird.vy
+    bird.vy += GRAVITY
+    bird.y += (uy + bird.vy) / 2
+    bird.x = 75
 
-    if bird.collidelist([wall_top, wall_bottom]) != -1:
+    if not bird.dead:
+        if bird.vy < -3:
+            bird.image = 'bird2'
+        else:
+            bird.image = 'bird1'
+
+    if bird.colliderect(wall_top) or bird.colliderect(wall_bottom):
         bird.dead = True
         bird.image = 'birddead'
 
     if not 0 < bird.y < 720:
-        bird.y = 50
+        bird.y = 200
         bird.dead = False
         bird.score = 0
-        bird.gravity = 5
+        bird.vy = 0
         reset_walls()
-
-
-def animate_bird():
-    if bird.dead:
-        return
-    elif bird.image == 'bird1':
-        bird.image = 'bird2'
-    else:
-        bird.image = 'bird1'
 
 
 def update():
@@ -68,26 +64,18 @@ def update():
 
 def on_key_down():
     if not bird.dead:
-        bird.jump = 17
-        bird.gravity = 5
-        bird.jumpSpeed = 10
-
-
-WHITE = 255, 255, 255
+        bird.vy = -FLAP_STRENGTH
 
 
 def draw():
-    screen.fill(WHITE)
     screen.blit('background', (0, 0))
     wall_top.draw()
     wall_bottom.draw()
     bird.draw()
     screen.draw.text(
         str(bird.score),
-        color=WHITE,
-        topright=(WIDTH - 10, 10),
-        fontsize=50
+        color='white',
+        midtop=(WIDTH // 2, 10),
+        fontsize=70,
+        shadow=(1, 1)
     )
-
-
-clock.schedule_interval(animate_bird, 0.1)
