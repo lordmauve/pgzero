4e32487cf504eea6b7f6ef6a76553876da33e666
Ian <244753+Avaren@users.noreply.github.com>
2016-03-07 21:13:11 +0000

    Generate tones on demand rather than using a pre-defined table

--- pgzero/tone.py
+++ pgzero/tone.py
@@ -1,4 +1,7 @@
+import re
 from functools import lru_cache
+
+import math
 import pygame
 import numpy
 import pygame.sndarray
@@ -163,6 +166,14 @@ TONE_MAP = {
     'B8': 7902.13
 }
 
+NOTE_PATTERN = r'^([A-G])([b#]?)([0-8])$'
+
+A4 = 440.0
+
+NOTE_VALUE = dict(C=-9, D=-7, E=-5, F=-4, G=-2, A=0, B=2)
+
+TWELTH_ROOT = math.pow(2, (1/12))
+
 
 def sine_array_onecycle(hz):
     """Returns a single sin wave for a given frequency"""
@@ -171,7 +182,8 @@ def sine_array_onecycle(hz):
     xvalues = numpy.arange(int(length)) * omega
     return (numpy.sin(xvalues) * (2 ** 15)).astype(numpy.int16)
 
-@lru_cache
+
+@lru_cache()
 def create(hz, length):
     """Creates a tone of a certain length from a note or frequency in hertz"""
     if isinstance(hz, str):
@@ -182,6 +194,30 @@ def create(hz, length):
     tone = numpy.resize(cycle, num_cycles)
     stereo = numpy.array(list(zip(tone, tone)))
     return pygame.sndarray.make_sound(stereo)
+
+class InvalidNote(Exception):
+    pass
+
+def note_to_hertz(note):
+    value = note_value(note)
+    return A4 * math.pow(TWELTH_ROOT, value)
+
+
+def note_value(note):
+    match = re.match(NOTE_PATTERN, note)
+    if match is None:
+        raise InvalidNote('%s is not a valid note, notes are A-F, are either normal, flat (b) or sharp (#) and of octave 0-8 ' % note)
+
+    note, accidental, octave = match.group(1, 2, 3)
+    value = NOTE_VALUE[note]
+    if accidental == 'b':
+        value -= 1
+    elif accidental == '#':
+        value += 1
+
+    return (4 - int(octave)) * -12 + value
+
+
 
 
 def play(hz, length):
