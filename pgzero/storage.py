b9a54cca8a7142354b28c3571d094eb2f8918e06
Daniel Pope <mauve@mauveweb.co.uk>
2019-05-07 16:56:55 +0100

    Report all storage serialisation errors, not just first

--- pgzero/storage.py
+++ pgzero/storage.py
@@ -1,10 +1,6 @@
 import json
 import os
 import platform
-import logging
-
-
-logger = logging.getLogger(__name__)
 
 
 __all__ = ['StorageCorruptionException', 'JSONEncodingException', 'Storage']
@@ -19,9 +15,6 @@ class JSONEncodingException(Exception):
 
 
 class Storage(dict):
-
-    path = ''
-
     """Behaves like a dictionary with a few extra functions.
 
     It's possible to load/save the data to disk.
@@ -31,8 +24,11 @@ class Storage(dict):
     NOTE: If two scripts have the same name, they will load/save from/to
     the same file.
     """
+
+    path = ''
+
     def __init__(self):
-        dict.__init__(self)
+        super().__init__()
 
         storage_path = self._get_platform_pgzero_path()
         if not os.path.exists(storage_path):
@@ -52,11 +48,11 @@ class Storage(dict):
                 data = json.load(f)
         except FileNotFoundError:
             # If no file exists, it's fine
-            logger.debug('No file to load data from.')
+            pass
         except json.JSONDecodeError:
-            msg = 'Storage is corrupted. Couldn\'t load the data.'
-            logger.error(msg)
-            raise StorageCorruptionException(msg)
+            raise StorageCorruptionException(
+                "Storage is corrupted. Couldn't load the data."
+            )
         else:
             self.update(data)
 
@@ -65,47 +61,69 @@ class Storage(dict):
         try:
             data = json.dumps(self)
         except TypeError:
-            json_path, key_type_obj = self._get_json_error_keys(self)
-            msg = 'The following entry couldn\'t be JSON serialized: storage{} of type {}'
-            msg = msg.format(json_path, key_type_obj)
-            logger.error(msg)
-            raise JSONEncodingException(msg)
+            msgs = [
+                "{} - type {}".format(*item)
+                for item in self._get_json_error_keys(self)
+            ]
+            raise JSONEncodingException(
+                "The following storage entries cannot be stored, because "
+                "they are not JSON serializable types:\n{}\n\n"
+                "Only the following types of objects are JSON-serialisable: "
+                "dict, list/tuple, str, float/int, bool, and None".format(
+                    "\n".join(msgs)
+                )
+            )
         else:
             with open(self.path, 'w+') as f:
                 f.write(data)
 
+    # Constants for use with isinstance in _get_json_error_keys()
+    JSON_PRIMITIVES = (float, int, str, bool, type(None))
+    JSON_SEQ = (list, tuple)
+    JSON_MAPPING = dict
+
     @classmethod
-    def _get_json_error_keys(cls, obj, json_path=''):
-        """Work out which part of the storage failed to be JSON encoded.
+    def _get_json_error_keys(cls, obj, json_path='storage'):
+        """Identify the paths with the storage which failed to be JSON encoded.
 
-        This function returns None if there is no error.
+        Return an iterable of pairs (path, typename).
+
         """
-
-        if type(obj) in (float, int, str, bool):
-            return None
-        elif isinstance(obj, list):
-            for i, value in enumerate(obj):
-                result = cls._get_json_error_keys(value, '{}[{}]'.format(json_path, i))
-                if result is not None:
-                    return result
-        elif isinstance(obj, dict):
+        if isinstance(obj, dict):
             for k, v in obj.items():
-                result = cls._get_json_error_keys(v, '{}[\'{}\']'.format(json_path, k))
-                if result is not None:
-                    return result
+                if isinstance(v, cls.JSON_PRIMITIVES):
+                    continue
+                subpath = '{}[{!r}]'.format(json_path, k)
+                yield from cls._get_json_error_keys(v, subpath)
+        elif isinstance(obj, (list, tuple)):
+            for i, value in enumerate(obj):
+                if isinstance(value, cls.JSON_PRIMITIVES):
+                    continue
+                subpath = '{}[{}]'.format(json_path, i)
+                yield from cls._get_json_error_keys(value, subpath)
+        elif isinstance(obj, cls.JSON_PRIMITIVES):
+            return
         else:
-            # TODO: Could have a custom JSONEncoder in the future, in which case we would
-            # have to actually run json.dumps on it to get the result and possibly even
-            # drill down on the parameters we got back, if that object happens to have
-            # a list as an attribute, for example.
-            return (json_path, type(obj))
-
-        # Explicitly returning None for the sake of code readability
-        return None
+            # TODO: Could have a custom JSONEncoder in the future, in which
+            # case we would have to actually run json.dumps on it to get the
+            # result and possibly even drill down on the parameters we got
+            # back, if that object happens to have a list as an attribute, for
+            # example.
+            t = type(obj)
+            if t.__module__ != 'builtins':
+                typename = '{t.__module__}.{t.__qualname__}'.format(t=t)
+            else:
+                typename = t.__qualname__
+            yield json_path, typename
 
     @staticmethod
     def _get_platform_pgzero_path():
-        """Attempts to get the right directory in Windows/Linux.MacOS"""
+        r"""Get the storage directory for pgzero save data.
+
+        Under Windows, return %APPDATA%\pgzero. Under Linux/MacOS, return
+        ~/.config/pgzero/saves.
+
+        """
         if platform.system() == 'Windows':
             return os.path.join(os.environ['APPDATA'], 'pgzero')
         return os.path.expanduser(os.path.join('~', '.config/pgzero/saves'))
--- test/test_storage.py
+++ test/test_storage.py
@@ -3,31 +3,45 @@ from unittest import mock
 from unittest.mock import patch
 
 from pgzero.storage import Storage
+from pgzero.rect import ZRect
 
 
 class StorageStaticMethodsTest(unittest.TestCase):
     def test_dict_with_no_errors(self):
         obj = {'level': 10, 'player_name': 'Daniel'}
 
-        result = Storage._get_json_error_keys(obj)
-        self.assertIsNone(result)
+        result = list(Storage._get_json_error_keys(obj))
+        self.assertEqual(result, [])
 
     def test_dict_with_errors(self):
         obj = {'level': 10, 'player_name': 'Daniel', 'obj': object()}
-        expected_result = ("['obj']", type(object()))
 
-        result = Storage._get_json_error_keys(obj)
-        self.assertEqual(result, expected_result)
+        result = list(Storage._get_json_error_keys(obj))
+        self.assertEqual(
+             result,
+             [("storage['obj']", "object")]
+        )
 
     def test_dict_with_nested_dicts_errors(self):
         subobj0 = {'this_key_fails': object()}
-        subobj1 = {'a': 10, 'b': 20, 'c': 30, 'obj': subobj0}
+        subobj1 = {'a': 10, 'b': {1, 2, 3}, 'c': 30, 'obj': subobj0}
         subobj2 = {'player_name': 'Daniel', 'level': 20, 'states': subobj1}
         obj = {'game': 'my_game', 'state': subobj2}
-        expected_result = ("['state']['states']['obj']['this_key_fails']", type(object()))
 
-        result = Storage._get_json_error_keys(obj)
-        self.assertEqual(result, expected_result)
+        result = sorted(Storage._get_json_error_keys(obj))
+        self.assertEqual(result, [
+            ("storage['state']['states']['b']", 'set'),
+            ("storage['state']['states']['obj']['this_key_fails']", 'object'),
+        ])
+
+    def test_invalid_list_item(self):
+        """We can report the index of an unserialisable list item."""
+        obj = {'items': [1, 5, ZRect(0, 0, 10, 10)]}
+
+        result = sorted(Storage._get_json_error_keys(obj))
+        self.assertEqual(result, [
+            ("storage['items'][2]", 'pgzero.rect.ZRect'),
+        ])
 
 
 class StorageTest(unittest.TestCase):
@@ -36,7 +50,7 @@ class StorageTest(unittest.TestCase):
         exists_mock.return_value = True
         self.storage = Storage()
 
+    @patch('builtins.open', mock.mock_open(read_data='{"a": "hello"}'))
     def test_get(self):
-        with patch('builtins.open', mock.mock_open(read_data='{"a": "hello"}')) as m:
-            self.storage.load()
-            self.assertEqual(self.storage['a'], 'hello')
+        self.storage.load()
+        self.assertEqual(self.storage['a'], 'hello')
