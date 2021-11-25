13c70ecc530741b97ac48976ff08f999cef8d634
Daniel Pope <mauve@mauveweb.co.uk>
2021-11-25 11:59:56 +0000

    Convert test names to snake case

--- test/test_rect_actor.py
+++ test/test_rect_actor.py
@@ -7,8 +7,8 @@ from pgzero.loaders import set_root
 from pgzero.rect import Rect
 
 # Check that Actor is compatible with PyGame Rect functions
-# Note that though the Rect module also contains ZRect, this has not yet been put
-# into use
+# Note that though the Rect module also contains ZRect, this has not yet been
+# put into use
 
 # All methods that take another Rect, no in place modification:
 
@@ -52,46 +52,46 @@ class RectActorTestSingularNoIp(unittest.TestCase):
         self.enclosed_rect = Rect((110, 160), (10, 10))
         self.enclosing_rect = Rect((0, 0), (500, 500))
 
-    def testUnionSeparate(self):
+    def test_union_separate(self):
         self.assertEquals(
             self.separate_rect.union(self.actor),
             Rect((0, 20), (166, 300))
         )
 
-    def testUnionOverlapping(self):
+    def test_union_overlapping(self):
         self.assertEquals(
             self.overlapping_rect.union(self.actor),
             Rect((100, 100), (120, 142))
         )
 
-    def testUnionEnclosed(self):
+    def test_union_enclosed(self):
         self.assertEquals(
             self.enclosed_rect.union(self.actor),
             Rect((100, 150), (66, 92))
         )
 
-    def testContainsTrue(self):
+    def test_contains_true(self):
         self.assertTrue(self.enclosing_rect.contains(self.actor))
 
-    def testContainsFalseOverlapping(self):
+    def test_contains_false_overlapping(self):
         self.assertFalse(self.overlapping_rect.contains(self.actor))
 
-    def testContainsFalseEnclosed(self):
+    def test_contains_false_enclosed(self):
         self.assertFalse(self.enclosed_rect.contains(self.actor))
 
-    def testContainsFalseSeparate(self):
+    def test_contains_false_separate(self):
         self.assertFalse(self.separate_rect.contains(self.actor))
 
-    def testCollideRectTrueEnclosing(self):
+    def test_collide_rect_true_enclosing(self):
         self.assertTrue(self.enclosing_rect.colliderect(self.actor))
 
-    def testCollideRectTrueEnclosed(self):
+    def test_collide_rect_true_enclosed(self):
         self.assertTrue(self.enclosed_rect.colliderect(self.actor))
 
-    def testCollideRectTrueOverlapping(self):
+    def test_collide_rect_true_overlapping(self):
         self.assertTrue(self.overlapping_rect.colliderect(self.actor))
 
-    def testCollideRectFalseSeparate(self):
+    def test_collide_rect_false_separate(self):
         self.assertFalse(self.separate_rect.colliderect(self.actor))
 
 
