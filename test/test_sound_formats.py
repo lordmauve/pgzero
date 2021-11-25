8e7c1850425c914ad92a5283ee9592d93ef39d17
Daniel Pope <mauve@mauveweb.co.uk>
2021-11-25 13:02:43 +0000

    Remove commented test skips

--- test/test_sound_formats.py
+++ test/test_sound_formats.py
@@ -40,31 +40,15 @@ class SoundFormatsTest(TestCase):
     def test_load_8kadpcm(self):
         self.assert_loadable('wav8kadpcm')
 
-    # @skip(
-    #     'This test crashes - reported upstream as '
-    #     'https://github.com/pygame/pygame/issues/406'
-    # )
     def test_load_11kgsm(self):
         self.assert_errmsg('wav22kgsm', 'WAV audio encoded as GSM')
 
-    # @skip(
-    #     'This test crashes - reported upstream as '
-    #     'https://github.com/pygame/pygame/issues/406'
-    # )
     def test_load_11kulaw(self):
         self.assert_loadable('wav22kulaw')
 
-    # @skip(
-    #     'This test crashes - reported upstream as '
-    #     'https://github.com/pygame/pygame/issues/406'
-    # )
     def test_load_8kmp316(self):
         self.assert_errmsg('wav8kmp316', 'WAV audio encoded as MP3')
 
-    # @skip(
-    #     'This test crashes - reported upstream as '
-    #     'https://github.com/pygame/pygame/issues/406'
-    # )
     def test_load_8kmp38(self):
         self.assert_errmsg('wav8kmp38', 'WAV audio encoded as MP3')
 
